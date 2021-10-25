67ae27b321de7c3e7ba0ab4edf841971cfded523
shashank-kumar-rajput <66230997+shashank-kumar-rajput@users.noreply.github.com>
2021-10-25 11:35:02 +0530

    Add files via upload

---
+++ testingpy.ut.py
@@ -0,0 +1,12 @@
+import unittest
+
+class testingpy(unittest.TestCase):
+ 
+  def test(self):
+   self.assertTrue(True)
+  def test_2(self):
+    self.assertTrue(True)
+  
+ 
+if __name__ == '__main__':
+ unittest.main()
